3033023c6baf7ffd112b64a6391db8b46297f22f
Mario Mol <mariohmol@gmail.com>
2020-02-01 02:21:15 -0300

    cleaning jade

--- demo/app.ts
+++ demo/app.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
